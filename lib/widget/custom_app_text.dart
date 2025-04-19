0f5aa3df9c7a73e78a1f01c52939dab77dc9f088
derafael <derafael04@gmail.com>
2025-04-19 00:13:30 -0300

    adiciona componente AppText com estilos de texto personalizados

---
+++ lib/widget/custom_app_text.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+enum AppTextStyleType {
+  display,
+  largeTitle,
+  title,
+  subtitle,
+  bodyLarge,
+  bodyStrong,
+  body,
+  caption,
+}
+
+class AppText extends StatelessWidget {
+  final String text;
+  final AppTextStyleType style;
+  final TextAlign? textAlign;
+  final Color? color;
+  final int? maxLines;
+  final TextOverflow? overflow;
+
+  const AppText({
+    super.key,
+    required this.text,
+    required this.style,
+    this.textAlign,
+    this.color,
+    this.maxLines,
+    this.overflow,
+  });
+
+  TextStyle _getTextStyle(BuildContext context) {
+    final defaultColor = Theme.of(context).textTheme.bodyMedium?.color;
+
+    switch (style) {
+      case AppTextStyleType.display:
+        return GoogleFonts.outfit(
+          fontSize: 40,
+          fontWeight: FontWeight.w700,
+          color: color ?? defaultColor,
+        );
+      case AppTextStyleType.largeTitle:
+        return GoogleFonts.outfit(
+          fontSize: 32,
+          fontWeight: FontWeight.w600,
+          color: color ?? defaultColor,
+        );
+      case AppTextStyleType.title:
+        return GoogleFonts.outfit(
+          fontSize: 24,
+          fontWeight: FontWeight.w600,
+          color: color ?? defaultColor,
+        );
+      case AppTextStyleType.subtitle:
+        return GoogleFonts.outfit(
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+          color: color ?? defaultColor,
+        );
+      case AppTextStyleType.bodyLarge:
+        return GoogleFonts.outfit(
+          fontSize: 18,
+          fontWeight: FontWeight.w400,
+          color: color ?? defaultColor,
+        );
+      case AppTextStyleType.bodyStrong:
+        return GoogleFonts.outfit(
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+          color: color ?? defaultColor,
+        );
+      case AppTextStyleType.body:
+        return GoogleFonts.outfit(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+          color: color ?? defaultColor,
+        );
+      case AppTextStyleType.caption:
+        return GoogleFonts.outfit(
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+          color: color ?? defaultColor,
+        );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: _getTextStyle(context),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      overflow: overflow,
+    );
+  }
+}
